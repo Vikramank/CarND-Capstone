8fd76847dea8e50418aa3a0c9c3f89b262523196
Vikramank <vikramank@hotmail.com>
2018-08-27 13:43:28 +0530

    Initial test

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,8 +3,9 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
+from scipy.spatial import KDTree
 import math
+import numpy as np
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -32,19 +33,46 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        rospy.Subscriber('/traffic_waypoint',Int32,self.traffic_cb )
+        rospy.Subscriber('/obstacle_waypoint',Lane,self.obstacle_cb )
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        waypoints
+        waypoint
+        velocity
+        self.pose=None
+        self.base_waypoints=None
+        self.waypoints_2d=None
+        self.waypoint_tree=None
+        self.loop()
 
-        rospy.spin()
+        #rospy.spin()
+    #loop to publish waypoint data at the rate of 50 Hz. Note, Carla will accept messages published at a rate greater than 10 Hz
+
+    def loop(self):
+        rate= rospy.Rate(50)
+        #run the following code till the system is live
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                closest_waypoint_idx=self.get_closest_waypoint_index() #get closest_index
+                self.publish_waypoints(closest_waypoint_idx)
+                rate.sleep()
 
     def pose_cb(self, msg):
         # TODO: Implement
+        self.pose=msg.pose #current position of the car
         pass
 
     def waypoints_cb(self, waypoints):
+        if not self.waypoints:
+              self.waypoints=waypoints #collect the base_waypoints of the system
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+        if (not self.waypoint_tree) and (not self.waypoints_2d) :
+              self.waypoint_tree=KDTree(self.waypoints_2d)
+
         # TODO: Implement
         pass
 
@@ -62,6 +90,23 @@ class WaypointUpdater(object):
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
+   def get_closest_waypoint_index(self.base_waypoints, self.pose):
+       current_pose=np.asarray([self.pose.pose.position.x,self.pose.pose.position.y])
+       _,closest_index=self.waypoint_tree.query(current_pose)
+       closest_coordinate=np.asarray(self.waypoints_2d[closest_index])
+       previous_coordinate=np.asarray(self.waypoints_2d[closest_index-1])
+       #check whether this waypoint is ahead or behind the car
+       a_vector=closest_coordinate-previous_coordinate
+       b_vector=current_pose-closest_coordinate
+       dot_product=np.dot(a_vector,b_vector)
+       bool=dot_product>0
+
+       if bool:
+            closest_index = (closest_index + 1) % len(self.waypoints_2d)
+
+       return closest_index
+
+
     def distance(self, waypoints, wp1, wp2):
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
@@ -69,6 +114,11 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+    def publish_waypoints(self, closest_index):
+        lane=Lane()
+        lane.header=self.waypoints.header
+        lane.waypoints=self.waypoints[closest_index:closest_index+LOOKAHEAD_WPS]
+        self.final_waypoints_pub(lane)
 
 
 if __name__ == '__main__':
