f5abfb17fcd336a4130ef58d26dea496ee286383
Vikramank <vikramank@hotmail.com>
2018-09-09 12:56:17 +0530

    Update tl_detector.py

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -156,14 +156,13 @@ class TLDetector(object):
         if(not self.has_image):
             self.prev_light_loc = None
             return False
-
+        """    
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-        """
 
         #Get classification
-        #return self.light_classifier.get_classification(cv_image)
+        return self.light_classifier.get_classification(cv_image)
         #return light.state
-        return TrafficLight.RED
+        #return TrafficLight.RED 
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
